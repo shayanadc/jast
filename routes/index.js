c69fa5997caa22b4bd1d0710ec242e9c76926333
shayan <shayanadc@gmail.com>
2019-10-15 15:50:03 +0330

    refactor htm test

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ const { check, validationResult } = require('express-validator');
 router.get('/',function(req, res) {res.render('index')})
 router.get('/all',function(req, res) {res.render('all')})
 router.get('/new',function(req, res) {res.render('new')})
+router.get('/testimo',function(req, res) {res.render('testimo')})
+
 
 async function ensureAuthenticated(req, res, next) {
  var Htoken = req.headers.access_token
