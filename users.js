62e777f3db69c1a219ca115ebf37cf7b5ece5a09
shayan <shayanadc@gmail.com>
2019-10-21 11:50:11 +0330

    form submit and refresh

--- users.js
+++ users.js
@@ -1,71 +1,87 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 mongoose.Promise = Promise;
-var { model, Schema } = mongoose
+var { model, Schema } = mongoose;
 
 var UserSchema = new Schema({
-  githubId : {
+  githubId: {
     type: String
   },
-  prefix : {
+  prefix: {
     type: String,
     unique: true
   },
-  access_token : {
-    type: String,
+  access_token: {
+    type: String
     // unique: true
   },
-  stubs : [{ type: Schema.Types.ObjectId, ref: 'Stub' }]
+  stubs: [{ type: Schema.Types.ObjectId, ref: "Stub" }]
 });
 function isJson(item) {
-  item = typeof item !== "string"
-      ? JSON.stringify(item)
-      : item;
-
+  item = typeof item !== "string" ? JSON.stringify(item) : item;
   try {
-      item = JSON.parse(item);
+    item = JSON.parse(item);
   } catch (e) {
-      return false;
+    return false;
   }
 
   if (typeof item === "object" && item !== null) {
-      return true;
+    return true;
   }
 
   return false;
+}
+function nullOrJson(item) {
+  if (item == "") {
+    return true;
+  } else {
+    item = typeof item !== "string" ? JSON.stringify(item) : item;
+    console.log(item);
+    try {
+      item = JSON.parse(item);
+    } catch (e) {
+      return false;
+    }
+
+    if (typeof item === "object" && item !== null) {
+      return true;
+    }
+
+    return false;
+  }
 }
 var StubSchema = new Schema({
   request: {
     type: Object,
     required: true,
-    validate: [isJson, 'request should be json type']
+    validate: [nullOrJson, "request should be json type"]
   },
   response: {
     type: Object,
     required: true,
-    validate: [isJson, 'response should be json type']
+    validate: [isJson, "response should be json type"]
   },
-  endpoint : {
+  endpoint: {
     type: String,
     required: true
   },
-  method : {
+  method: {
     type: String,
     required: true
   },
-  header : Object,
-  status : {
+  header: Object,
+  status: {
     type: Number,
     min: 200,
     max: 499,
     required: true
   },
-  user : { type: Schema.Types.ObjectId, ref: 'User' }
+  user: { type: Schema.Types.ObjectId, ref: "User" }
 });
 
-UserSchema = model('User', UserSchema);
-StubSchema = model('Stub', StubSchema);
+UserSchema = model("User", UserSchema);
+StubSchema = model("Stub", StubSchema);
 
 module.exports = {
   UserSchema,
-  StubSchema,
-}
+  StubSchema
+};
